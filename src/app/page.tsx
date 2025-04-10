3d673895a3e5aca1b178310d6df55216aa568a6b
Théo <12082276+murgatt@users.noreply.github.com>
2025-04-10 14:54:37 +0200

    feat: simplify base styles (#67)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function HomePage() {
   const t = useTranslations('HomePage');
 
   return (
-    <main className="gap min-h-screen p-24 text-center">
+    <main className="gap flex min-h-screen flex-col justify-center text-center">
       <h1 className="text-2xl font-semibold">{t('title')}</h1>
     </main>
   );
